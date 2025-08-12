d7401d57b844bdd9a4bb529968bc8bd811f1ad1b
Данил Коньшин <d.konshin@lantan.com>
2025-08-12 17:08:27 +0700

    canvas-controlls set up canvas main events and grid component

---
+++ src/shared/model/BaseSceneElement.js
@@ -0,0 +1,13 @@
+import { v4 as uuidv4 } from "uuid";
+
+export class BaseSceneElement {
+  constructor(id = uuidv4()) {
+    this.id = id;
+  }
+
+  draw(ctx) {}
+
+  getId() {
+    return this.id;
+  }
+}
